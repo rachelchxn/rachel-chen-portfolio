68114b7ad969e485d140ae413d9ab9d607fad41d
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2023-08-21 01:41:43 -0400

    dev stuff

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,45 +1,55 @@
 import Image from "next/image";
-import styles from "./design.module.scss";
+import styles from "./dev.module.scss";
 import Navigation from "@/components/navigation";
 import ProjectBlock from "@/components/project-block";
 import Footer from "@/components/footer";
+import DevCard from "@/components/devCard";
+
+const projects = [
+  {
+    title: "ASL Translator",
+    description: "Bridging Communication Gaps With Machine Learning and Computer Vision",
+    tags: ["AI", "Computer Vision", "Python"],
+    imageURL: "dev-work.svg",
+    colour: "#E2EDF6"
+  },
+  {
+    title: "ASL Translator",
+    description: "Bridging Communication Gaps With Machine Learning and Computer Vision",
+    tags: ["AI", "Computer Vision", "Python"],
+    imageURL: "dev-work.svg",
+    colour: "#E2EDF6"
+  },
+  {
+    title: "ASL Translator",
+    description: "Bridging Communication Gaps With Machine Learning and Computer Vision",
+    tags: ["AI", "Computer Vision", "Python"],
+    imageURL: "dev-work.svg",
+    colour: "#E2EDF6"
+  }
+]
 
 export default function Dev() {
   return (
     <main className={styles.main}>
       <div className={styles.projects}>
         <div className={styles.header}>
-          <h2>I code.</h2>
+          <h2>I develop things that matter.</h2>
           <p>
-            I’m a product designer with front-end development skills and a
-            background in branding design. I have a burning passion for creating
-            intuitive experiences in high-impact products.
+            From tackling mental illness, solving sustainability issues, and taking on real business problems, I code for the potential technology has to aid and enrich our lives. Also, it’s fun.
           </p>
         </div>
-        <ProjectBlock
-          title="Earth"
-          description="A Hackathon Management Platform for Fortune 500 Companies"
-          tags={["UX Design", "User Testing", "Front-End Development"]}
-          imageURL="/earth-image.svg"
-          order={true}
-          colour="#E2EAFA"
-        />
-        <ProjectBlock
-          title="PokerGPT"
-          description="The World’s First AI-Powered Poker Coach"
-          tags={["UX Design", "Brand Design", "Front-End Development"]}
-          imageURL="/pokergpt-image.svg"
-          order={false}
-          colour="#E5DEF8"
-        />
-        <ProjectBlock
-          title="The Cups"
-          description="Frozen Dessert-On-Demand for Everyone"
-          tags={["UX Design", "UI Design"]}
-          imageURL="/thecups-image.svg"
-          order={true}
-          colour="#FCE2E7"
-        />
+        <div className={styles.devGrid}>
+          {projects.map((project) => (
+            <DevCard
+              title={project.title} 
+              description={project.description}
+              tags={project.tags}
+              imageURL={project.imageURL}
+              colour={project.colour}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
---
+++ src/components/devCard.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./dev-card.module.scss";
+import { useRouter } from "next/navigation";
+
+type DevCardProps = {
+  title: string;
+  description: string;
+  tags: string[];
+  imageURL: string;
+  colour: string;
+};
+
+const DevCard: React.FC<DevCardProps> = ({
+  title,
+  description,
+  tags,
+  imageURL,
+  colour,
+}) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    const formattedTitle = title
+      .toLowerCase()
+      .replace(/\s+/g, "-")
+      .replace(/[^a-z0-9-]/g, "");
+    router.push(`/dev/${formattedTitle}`);
+  };
+
+  return (
+    <div className={styles.card} onClick={handleClick}>
+        <div className={styles.imageBlock}>
+            <img className={styles.image} src={`${imageURL}`} />
+        </div>
+        <div className={styles.info} style={{ backgroundColor: colour }}>
+            <div className={styles.infoMain}>
+                <h5>{title}</h5>
+                <h3>{description}</h3>
+            </div>
+            <div>
+            <div className={styles.tagList}>
+                {tags.map((tag, index) => (
+                    <p key={index} className={styles.tag}>
+                    {tag}
+                    </p>
+                ))}
+            </div>
+            <div className={styles.arrow}>
+                <Image src="arrow.svg" alt="->" width={32} height={32} />
+            </div>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default DevCard;
