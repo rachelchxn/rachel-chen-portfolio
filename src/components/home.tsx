201101be3fee1682e18222fa5896b58a685fab74
Rachel Chen <rchen457@uwo.ca>
2024-11-14 12:42:46 -0500

    + intuitive

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -35,8 +35,8 @@ export default function HomePage() {
               maxWidth: "460px",
             }}
           >
-            I'm a 0 -{">"} 1 product builder who ships delightful experiences
-            that empower users.
+            I'm a 0 -{">"} 1 product builder who ships intuitive + delightful
+            experiences that empower users.
           </h2>
         </div>
 
